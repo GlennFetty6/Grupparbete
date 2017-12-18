83bdb5c776f2955ad11b97f5e7bf17e10e0c4c0f
EllaKs <elenoreknutsson@gmail.com>
2017-12-18 15:12:33 +0100

    Add files via upload

--- LogInForm.cs
+++ LogInForm.cs
@@ -20,24 +20,22 @@ namespace DigitCashier
 
         private void LogIn_Click(object sender, EventArgs e)
         {
-            //DataSet ds = new DataSet();
-            //SqlConnection connect = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=DigitLogin;Integrated Security=True");
-            //SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From EmployeeLogin where UserID= '" + userIDTxtbox.Text + "' and Password= '" + passwordTxtbox.Text + "'", connect);
-            //sda.Fill(ds);
+                DataSet ds = new DataSet();
+                SqlConnection connect = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=DigitLogin;Integrated Security=True");
+                SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From EmployeeLogin where UserID= '" + userIDTxtbox.Text + "' and Password= '" + passwordTxtbox.Text + "'", connect);
+                sda.Fill(ds);
 
-            //if (ds.Tables[0].Rows[0][0].ToString() == "1")
-            if (10 < 100)
-            {
-                Hide();
-                Inloggning.kodID = userIDTxtbox.Text;
-                Inloggning.LoggaIn(userIDTxtbox.Text);
-            }
+                if (ds.Tables[0].Rows[0][0].ToString() == "1")
+                {
+                    Hide();
+                    Inloggning.kodID = userIDTxtbox.Text;
+                    Inloggning.LoggaIn(userIDTxtbox.Text);
+                }            
 
-            else
-            {
-                errorMessage(); // Anropar felmeddelande
-            }
-        }
+        
+                    errorMessage(); // Anropar felmeddelande
+    }
+
         private void logInBtn_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
